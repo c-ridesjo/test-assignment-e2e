71978a19fe246ee0b7cd8118f65e0091bd63eed3
Camilla Ridesjö <camilla.ridesjo@gmail.com>
2023-02-07 19:56:11 +0100

    Add a test

--- cypress/e2e/movies.cy.ts
+++ cypress/e2e/movies.cy.ts
@@ -1,11 +1,29 @@
+beforeEach(() => {
+  cy.visit("http://localhost:1234");
+});
+
 describe('movies application', () => {
   it('passes', () => {
-    cy.visit("http://localhost:1234");
+    //cy.visit("http://localhost:1234");
   });
 
   it("should contain a button", () => {
-    cy.visit("http://localhost:1234");
+    //cy.visit("http://localhost:1234");
 
     cy.get("button").contains("Sök").should("exist");
+  });
+
+  it("should be able to type", () => {
+
+    cy.get("input").type("Hallå").should("have.value", "Hallå");
+  });
+
+  it("should find movies", () => {
+
+    cy.get("input").type("Finding Nemo").should("have.value", "Finding Nemo");
+
+    cy.get("button").contains("Sök").click();
+
+
   });
 });
