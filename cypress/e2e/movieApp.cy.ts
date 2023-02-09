2eea38707bf901f90404234590e7649adc2857c3
Camilla Ridesjö <camilla.ridesjo@gmail.com>
2023-02-09 10:41:40 +0100

    Add more tests

--- cypress/e2e/movieApp.cy.ts
+++ cypress/e2e/movieApp.cy.ts
@@ -2,6 +2,7 @@ beforeEach(() => {
   cy.visit("/");
 });
 
+/*
   it('passes', () => {
   });
 
@@ -50,14 +51,14 @@ describe('tests for input and button', () => {
     cy.get("input").type("Rambo").should("have.value","Rambo");
     cy.get("button").click();
     cy.get("movie-container").should("have.length", 5);
-  }); */
+  }); 
 
-});
+}); */
 
 describe('tests with mock', () => {
 
   it("should get mock data", () => { 
-    cy.intercept("GET", "http://omdbapi.com/*",    // stämmer URL?
+    cy.intercept("GET", "http://omdbapi.com/?apikey=416ed51a&s=/*",    // stämmer URL?
     {"title": "Perfect stranger"}).as ("omdbCall");
     cy.get("input").type("Perfect stranger");
     cy.get("button").click();
@@ -65,12 +66,44 @@ describe('tests with mock', () => {
     cy.wait("@omdbCall").its("request.url").should("contain", "Perfect stranger");
   });
 
-  it("should get mock data with correct url", () => { 
-    cy.intercept("GET", "http://omdbapi.com/*",    // stämmer URL?
-    { fixture:"omdbResponse" }).as ("omdbCall");
+  it("should get mock data with correct url", () => {   //samma test som lektionen i dag
+    cy.intercept("GET", "http://omdbapi.com/?apikey=416ed51a&s=/*",    // stämmer URL?
+    { fixture:"movies" }).as ("omdbCall");
+    
+  //skriv 8 i testrutan
+
+  //klicka på knappen sök
+
     cy.get("input").type("Perfect stranger");
     cy.get("button").click();
 
     cy.wait("@omdbCall").its('request.url').should("contain", "Perfect stranger");
+
+    //kontrollera html baserat på vår fixture
   });
 });
+
+
+// test lektion torsdag - mocks
+
+it("should create resource correctly", ()=> {
+  cy.intercept("POST", "https://swapi.dev/api/person/*", {success: true});  //happy flow
+
+  //fyll i formulär
+
+  // klicka på spara-knappen
+
+  // kontrollera att vår ok-ruta syns
+});
+
+it("should create resource correctly", ()=> {
+  cy.intercept("POST", "https://swapi.dev/api/people/*", {success: false, error:
+  "Error message from api regarding the request"});  
+
+  //fyll i formulär
+
+  // klicka på spara-knappen
+
+  // kontrollera att vår ok-ruta syns
+});
+
